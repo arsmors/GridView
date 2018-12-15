19068520bd971fe6ff3474b0ceb22f068b05354d
Arsens <arsens.morins@chi.lv>
2018-12-15 09:10:37 +0200

    first

---
+++ app/src/main/java/com/example/den/lesson3/Cheese.java
@@ -0,0 +1,17 @@
+package com.example.den.lesson3;
+
+/**
+ * Created by den on 3/25/18.
+ */
+
+class Cheese {
+    String name;
+    String description;
+    String info;
+
+    public Cheese(String name, String description, String info) {
+        this.name = name;
+        this.description = description;
+        this.info = info;
+    }
+}
